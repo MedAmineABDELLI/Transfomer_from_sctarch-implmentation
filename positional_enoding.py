b0f36d30f2bcef8bd3d47ae25344ba3f79dd0650
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-07-18 16:00:40 +0100

    coding-pos-encoding

--- positional_enoding.py
+++ positional_enoding.py
@@ -12,6 +12,15 @@ def postional_encoding_word(word , sequence_of_tokens):
         else:
             dim[i] = np.cos((pos / 1000**(2*i/len(dim))))
 
-def postional_encoding_sentence(dim):
-    
+def postional_encoding_sentence(dim,sequence_of_tokens):
+    PE_matrix = np.array(dim , dim)
+    for i in sequence_of_tokens:
+        PE_matrix[dim:] = postional_encoding_word(i,sequence_of_tokens)
+    return PE_matrix
+
+
+
+sequence_cleaned_tokens = ["le" , "chat" , "dort", "bien"]
+PE = postional_encoding_sentence(len(sequence_cleaned_tokens) ,  sequence_of_tokens=sequence_cleaned_tokens  )
+print(PE)
 
