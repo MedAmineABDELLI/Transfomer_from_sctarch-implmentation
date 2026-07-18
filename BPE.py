237f1a3d48076ebbf041de3b4826ab16764ad42a
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-07-18 18:46:05 +0100

    BPE-encoding

---
+++ BPE.py
@@ -0,0 +1,17 @@
+
+
+cratcters = {'h' , 'u' , 'g' , 'p' , 'n' , 's' , '$'}
+corpus = [
+    "hug$"
+    "pug$"
+    "pun$"
+    "hugs$"
+    ]
+
+def caracter_count(corpus):
+    for i in range(len(corpus)):
+        word = corpus[i]
+        for j in word:
+            carac_1 = j
+            for k in word:
+                if carac_1 == 
