7c390fbcc937fb1035d2aa8631fb122c7f9e795b
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-07-18 15:43:48 +0100

    coding-postional-encodig

---
+++ matrix_opertations.py
@@ -0,0 +1,6 @@
+import numpy as np
+def matrice_multiplication_numpy(matrix_A , matrix_B):
+    return np.matmul(matrix_A , matrix_B)
+
+def matrix_mul_scratch(matrix_A , matrix_B):
+    for 
---
+++ positional_enoding.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def postional_encoding_word(word , sequence_of_tokens):
+    dim = np.zeros(len(sequence_of_tokens))
+    for i in range(len(sequence_of_tokens)):
+        if word == sequence_of_tokens[i]:
+            pos = i
+            break
+    for i in range(len(sequence_of_tokens)):
+        if i % 2 == 0:
+            dim[i] = np.sin((pos / ))
+
