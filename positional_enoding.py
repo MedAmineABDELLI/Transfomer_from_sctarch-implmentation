88b58df7686c01591156ef69557d83cdd2427927
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-07-18 17:35:10 +0100

    coding-pos-encoding

--- positional_enoding.py
+++ positional_enoding.py
@@ -1,26 +1,22 @@
 import numpy as np
 
-def postional_encoding_word(word , sequence_of_tokens):
+def postional_encoding_word(sequence_of_tokens):
     dim = np.zeros(len(sequence_of_tokens))
+    PE_matrix = np.zeros((len(sequence_of_tokens) , len(sequence_of_tokens)))
     for i in range(len(sequence_of_tokens)):
-        if word == sequence_of_tokens[i]:
-            pos = i
-            break
-    for i in range(len(sequence_of_tokens)):
-        if i % 2 == 0:
-            dim[i] = np.sin((pos / 1000**(2*i/len(dim))))
+        pos = i
+        for j in range(len(sequence_of_tokens)):
+            if j % 2 == 0:
+                dim[i] = np.sin((pos / 1000**(2*j/len(dim))))
+                PE_matrix[pos,j] = dim[i]
         else:
-            dim[i] = np.cos((pos / 1000**(2*i/len(dim))))
+            dim[i] = np.cos((pos / 1000**(2*j/len(dim))))
+            PE_matrix[pos,j] = dim[i]
 
-def postional_encoding_sentence(dim,sequence_of_tokens):
-    PE_matrix = np.array(dim , dim)
-    for i in sequence_of_tokens:
-        PE_matrix[dim:] = postional_encoding_word(i,sequence_of_tokens)
-    return PE_matrix
+        return PE_matrix
 
 
-
-sequence_cleaned_tokens = ["le" , "chat" , "dort", "bien"]
-PE = postional_encoding_sentence(len(sequence_cleaned_tokens) ,  sequence_of_tokens=sequence_cleaned_tokens  )
+tokens = ["le" , "chat" , "dore" , "bien"]
+PE = postional_encoding_word(sequence_of_tokens=tokens)
 print(PE)
 
