aaffba5057af44b9d3836962729b63aef1c2598b
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-09-04 17:34:53 +0100

    att

--- matrix_opertations.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def matrice_multiplication_numpy(matrix_A , matrix_B):
-    return np.matmul(matrix_A , matrix_B)
-
-def matrix_mul_scratch(matrix_A , matrix_B):
-    for 
--- positional_enoding.py
+++ positional_enoding.py
@@ -1,22 +1,48 @@
 import numpy as np
+tokenised_texte = ["the" , "cat" , "is" , "so" ]
+d_model = len(tokenised_texte)
+def Postionnal_encoding_word(tokenised_test,word_postion):
+    dinominator = []
+    PE_arrray = []
+    index = 0
+    for i in range(0,len(tokenised_texte),2):
+        k = 2*index / d_model
+        denom = 1000**k
+        dinominator.append(denom)
+        index = index + 1
+    index = 0
+    for j in range(0,len(tokenised_texte)):
+        if j % 2 == 0:
+            PE_arrray.append(np.sin(word_postion) / dinominator[index])
+        if j % 2 != 0:
+            PE_arrray.append(np.cos(word_postion) / dinominator[index])
+            index = index + 1
 
-def postional_encoding_word(sequence_of_tokens):
-    dim = np.zeros(len(sequence_of_tokens))
-    PE_matrix = np.zeros((len(sequence_of_tokens) , len(sequence_of_tokens)))
-    for i in range(len(sequence_of_tokens)):
-        pos = i
-        for j in range(len(sequence_of_tokens)):
-            if j % 2 == 0:
-                dim[i] = np.sin((pos / 1000**(2*j/len(dim))))
-                PE_matrix[pos,j] = dim[i]
-        else:
-            dim[i] = np.cos((pos / 1000**(2*j/len(dim))))
-            PE_matrix[pos,j] = dim[i]
+
+    for i in range(0,len(PE_arrray)):
+        PE_arrray[i] = float(PE_arrray[i])
+
+
+    return PE_arrray
+
+
+
+
+
+
+
+def generate_postionnal_encoding_matrix(tokenised_texte):
+    PE_matrix = []
+    for i in range(0,len(tokenised_texte)):
+        index = 0
+        for j in range(0,len(tokenised_texte)):
+            word_embdeing_vector = Postionnal_encoding_word(tokenised_texte,word_postion=index)
+            #print(word_embdeing_vector)
+            PE_matrix.append(word_embdeing_vector)
+            index = index + 1
 
         return PE_matrix
 
 
-tokens = ["le" , "chat" , "dore" , "bien"]
-PE = postional_encoding_word(sequence_of_tokens=tokens)
-print(PE)
+
 
