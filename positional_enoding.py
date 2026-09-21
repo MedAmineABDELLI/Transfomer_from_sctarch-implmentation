00faea053ec323adf3d5c03bd326cda2eddee91c
MedAmineABDELLI <amineamineamine24oz@gmail.com>
2026-09-21 21:23:01 +0100

    fixed-postionnal-encoding-now-coding-decoder-attention

--- positional_enoding.py
+++ positional_enoding.py
@@ -4,6 +4,13 @@ import numpy as np
 tokenised_texte = ["the", "cat", "is", "so"]
 shifted_pos_text = ["SOS", "the", "cat", "is"]
 
+np.random.default_rng(42)
+d_model = 4
+Xe = np.random.rand(5,d_model)
+Wq = np.random.rand(d_model,d_model)
+Wk = np.random.rand(d_model,d_model)
+Wv = np.random.rand(d_model,d_model)
+
 d_model = len(shifted_pos_text)  # d_model = 4
 
 
@@ -59,6 +66,6 @@ PE_matrix = generate_postionnal_encoding_matrix(
 for row in PE_matrix:
     print([round(x, 4) for x in row])
 
-
+print(Xe)
 
 
